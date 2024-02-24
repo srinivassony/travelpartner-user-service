39763bfdfe9d5ade82008da93fe29564598cdc24
Srinivas kuntukulangara <srinivas.kuntukulangara@bluepal.com>
2024-02-25 00:59:19 +0530

    bug fix

--- src/database/db/user.js
+++ src/database/db/user.js
@@ -1,10 +1,40 @@
 const User = require("../../database/model/user");
 
-let getUserDetails = async () =>
+let getExsitingUserDetails = async (email, phone) =>
 {
-  return await User.query().select();
+  return await User.query().select().where('email', email).first();
+}
+
+let createUser = async(data) =>
+{
+    return await User.query().insert(data);
+}
+
+let getUserLoginDetails = async (email) => 
+{
+  return await User.query().select().where({email: email}).first()
+}
+
+let getUserDetailsById = async(id) =>
+{
+  return await User.query().select().where('id', id).first();
+}
+
+let updateUser = async (id, data) =>
+{
+    return await User.query().patchAndFetchById(id, data);
+}
+
+let getUserByEmailId = async(email) =>
+{
+  return await User.query().select().where('email', email).first();
 }
 
 module.exports ={
-  getUserDetails : getUserDetails
+  getExsitingUserDetails : getExsitingUserDetails,
+  createUser: createUser,
+  getUserLoginDetails: getUserLoginDetails,
+  getUserDetailsById: getUserDetailsById,
+  updateUser: updateUser,
+  getUserByEmailId: getUserByEmailId
 }
